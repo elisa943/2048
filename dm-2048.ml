571f74c70d6db48985b438d32204c8508ffa9511
elisa <68431568+elisa943@users.noreply.github.com>
2021-12-01 11:46:47 +0100

    Add files via upload

---
+++ dm-2048.ml
@@ -0,0 +1,29 @@
+#load "graphics.cma";;
+open Graphics;;
+open Random;;
+open List;;
+
+open_graph "800x600";;
+
+(* Fonctions pour tracer le background (titre, score) *)
+
+(* Structures pour les blocs : sous la forme d'un tableau (listes) *)
+
+(* Fonctions qui modifie le tableau + score *)
+
+(* Fonctions qui lit le tableau et l'affiche + affiche le background *)
+
+	(* tracer les lignes et contours, ajouter la couleur, les blocs, les nombres *)
+
+(* Fonctions qui choisit un emplacement libre random et choisit 2 ou 4 *)
+
+(* Fonctions qui détecte le game over *)
+
+(* Fonction qui affiche l'écran de game over *)
+
+
+(* Fonction main *)
+
+	(* fonction qui détecte le déplacement du joueur (keyboard) *)
+    
+    (* fonction *)
