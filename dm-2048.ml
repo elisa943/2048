b050b4aafc49dcd290ea5337be99c1f3c5f1e489
elisa <68431568+elisa943@users.noreply.github.com>
2021-12-12 11:06:25 +0100

    Add files via upload

--- dm-2048.ml
+++ dm-2048.ml
@@ -1,5 +1,6 @@
 open Graphics;;
 open Random;;
+open Array;;
 open List;;
 
 (* https://htmlcolors.com/google-color-picker *)
@@ -12,12 +13,13 @@ resize_window 800 600;;
 (* Fonctions pour tracer le background (titre, score) *)
 
 let bleu = rgb 15 140 171;;
-
 let rouge = rgb 224 20 64;;
+let la3 = sound 440 1;;
+
 
 let titre = 
 	moveto 50 500;
-	set_color bleu;
+	set_color rouge;
     set_text_size 50;
 	set_font "Avenir Next";
 	draw_string "2048";;
@@ -26,9 +28,13 @@ let point x y nb =
 	(* sert à obtenir un certain paramètre d'un couple de coordonnées *)
 	if nb = 0 then x else y;;
 
+let f = 
+let ff = ref 3 in !ff;;
+
+
+
 let lignes = 
-	set_line_width = 600;
-    moveto 200 0;
+	let set_line_width = 600 and moveto 200 0 in
     for i = 1 to 4 do
     	begin
 	    lineto 200*i (point current_point 1 + set_line_width);
@@ -44,11 +50,13 @@ close_graph;;
 clear_graph;;
 
 (* Structures pour les blocs : sous la forme d'un tableau (listes) *)
-cases = [ [0, 0, 0, 0],
-					[0, 0, 0, 0],
-					[0, 0, 0, 0],
-					[0, 0, 0, 0]  ]
 
+let cases = make_matrix 4 4 0;;
+let detecte
+
+
+let f = 4
+;;
 (* Fonctions qui modifie le tableau + score *)
 
 (* Fonctions qui lit le tableau et l'affiche + affiche le background *)
@@ -61,29 +69,39 @@ cases = [ [0, 0, 0, 0],
 
 (* Fonction qui affiche l'écran de game over *)
 
-let test_vide tableau = 
-		for i = 0 to 3 do	
-			for j = 0 to 3 do
-				if tableau.(i)(j) != 0 then false
-		
-		true
+let array_vide array = 
+	for i = 0 to 3 do	
+		for j = 0 to 3 do
+			if !array.(i).(j) != 0 then false 
+		done;
+	done;
 ;;
 
-let game_over = clear_graph
-
-if test_vide tableau then game_over else __
-
+let game_over = clear_graph;;
 
 (* Fonction main *)
 
+
+let game = 
+	let over = ref false 
+		and tableau = ref make_matrix 4 4 0 in
+	while !over do 
+	begin 
+    if array_vide tableau then 3 else 4
+	end
+    done;;
+
 	(* fonction qui détecte le déplacement du joueur (keyboard) *)
     
-Key_pressed;;
-
+Key_pressed;; 
 read_key;;
 
-let la3 = sound 440 1;;
+let test = let boo = ref true in
+while boo do
+	if Key_pressed = true 
+    	then read_key
+		else "over"
+    done;
+;;
 
-    
-    
-    (* fonction *)
+
