7e9b9daac6b2772b0279c1776a8b9a3cb793bf12
elisa <68431568+elisa943@users.noreply.github.com>
2021-12-18 18:31:18 +0100

    Add files via upload

--- dm-2048.ml
+++ dm-2048.ml
@@ -3,12 +3,13 @@
 open Graphics;;
 open Random;;
 open Array;;
-open List;;
 
 (* https://htmlcolors.com/google-color-picker *)
 (* https://caml.inria.fr/pub/docs/manual-caml-light/node16.html
 	 https://musicordes.fr/tableau-frequences-notes/
 *)
+
+(* 150x150 *)
 
 open_graph ":0";;
 
@@ -19,29 +20,35 @@ resize_window 800 600;;
 let bleu = rgb 38 92 153;;
 let rouge = rgb 224 20 64;;
 let vert = rgb 27 133 3;;
-let orange = rgb 222 118 20;;
+let rouge_fonce = rgb 135 42 8;;
+
 let la3 = sound 440 1;;
 
 
 
 let background = 
-	set_color bleu;
+	set_color rouge_fonce;
 	fill_rect 0 0 800 600;
-	moveto 50 500;
-	set_color rouge;
+	moveto 15 500;
+	set_color white;
   set_text_size 50;
 	draw_string "2048";
-	moveto 50 300;
-	set_text_size 30;
+	moveto 25 300;
+	set_text_size 25;
 	draw_string "Score :";
+	moveto 15 30;
+	set_text_size 15;
+	draw_string "Made by Elisa";
+
+	moveto 200 0;
 	for i = 1 to 4 do
 		begin
-			moveto (199*i) 0;
-			lineto (199*i) 600;
+			moveto (160*i) 0;
+			lineto (160*i) 600;
+			moveto 160 (150*i);
+			lineto 800 (150*i);
 		end
 	done;;
-	
-	
 	
 	
 
@@ -57,27 +64,62 @@ clear_graph;;
 
 (* Structures pour les blocs : sous la forme d'un tableau (listes) *)
 
-let cases = make_matrix 4 4 0;;
+
 
 (* Fonctions qui modifie le tableau + score *)
 
 (* Fonctions qui lit le tableau et l'affiche + affiche le background *)
 
+
+let read_and_print array = 
+	set_text_size 10;
+	
+	for i = 0 to 3 do
+		for j = 0 to 3 do
+			begin
+			moveto x y ;
+			set_color shade;
+			fill_rect x y w h
+			set_color shade;
+			draw_string char_of_int array.(i).(j);
+			end
+		done
+	done;;
+	
+
 	(* tracer les lignes et contours, ajouter la couleur, les blocs, les nombres *)
 
 (* Fonctions qui choisit un emplacement libre random et choisit 2 ou 4 *)
 
+let deux_ou_quatre = if (int 2) = 0 then 2 else 4;;
+
+let emplacements_libres array = 
+	let array_prime = make_matrix 4 4 0 in
+	for i = 0 to 3 do	
+		for j = 0 to 3 do
+			if array.(i).(j) != 0 then array_prime.(i).(j) <- 1
+			else array_prime.(i).(j) <- 0
+		done;
+	done;
+	array_prime;;
+
 (* Fonctions qui détecte le game over *)
+
+let array_vide array_prime = let vide = ref false and boo = ref true in
+	while !boo do
+	begin
+	for i = 0 to 3 do	
+		for j = 0 to 3 do
+			if array_prime.(i).(j) <> 0 then boo := false 
+			else boo := true
+		done;
+	done;
+	end;
+	!vide;;
 
 (* Fonction qui affiche l'écran de game over *)
 
-let array_vide array = 
-	for i = 0 to 3 do	
-		for j = 0 to 3 do
-			if !array.(i).(j) != 0 then false 
-		done;
-	done;
-;;
+
 
 let game_over = clear_graph;;
 
@@ -86,9 +128,19 @@ let game_over = clear_graph;;
 
 let game = 
 	let over = ref false 
-		and tableau = ref make_matrix 4 4 0 in
+	and tableau = make_matrix 4 4 0 in
 	while !over do 
 	begin 
+		if Key_pressed 
+		then fleche = ref read_key 
+		else fleche = ref Null
+		
+		if read 
+		
+	
+	
+	
+	
     if array_vide tableau then 3 else 4
 	end
     done;;
