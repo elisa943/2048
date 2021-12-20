6d9efaabb897bce9fbb192325a13ef53d031a995
elisa <68431568+elisa943@users.noreply.github.com>
2021-12-20 17:32:00 +0100

    Add files via upload

--- dm-2048.ml
+++ dm-2048.ml
@@ -17,7 +17,13 @@ let bleu = rgb 38 92 153;;
 let rouge = rgb 224 20 64;;
 let vert = rgb 27 133 3;;
 let rouge_fonce = rgb 135 42 8;;
-
+let bleu_clair = rgb 80 199 199;;
+let vert_clair = rgb 38 145 108;;
+let rose = rgb 150 47 100;;
+let bleu_fonce = rgb 47 39 138;;
+let violet = rgb 142 115 222;;
+let orange = rgb 212 133 42;;
+let bleu_ciel = rgb 42 206 212;;
 let la3 = sound 440 1;;
 
 (* Fonctions pour tracer le background (titre, score) *)
@@ -28,27 +34,25 @@ let initialisation =
 	resize_window 800 600;
 	set_color rouge_fonce;
 	fill_rect 0 0 800 600;
-	moveto 15 500;
+	moveto 35 500;
 	set_color white;
   set_text_size 50;
 	draw_string "2048";
-	moveto 25 300;
+	moveto 35 300;
 	set_text_size 25;
 	draw_string "Score :";
 	moveto 15 30;
 	set_text_size 15;
 	draw_string "Made by Elisa";
-	moveto 200 0;
-	for i = 1 to 4 do
+
+	for i = 0 to 3 do
 		begin
-			moveto (160*i) 0;
-			lineto (160*i) 600;
-			moveto 160 (150*i);
+			moveto (200+150*i) 0;
+			lineto (200+150*i) 600;
+			moveto 200 (150*i);
 			lineto 800 (150*i);
 		end
 	done;;
-
-fill_rect 0 0 160 150;;	
 
 (* Fonctions de générateurs aléatoires *)
 let deux_ou_quatre = if (int 2) = 0 then 2 else 4;;
@@ -59,54 +63,89 @@ let debut array array_prime = let a = int 4 and b = int 4
 		array.(a).(b) <- deux_ou_quatre;
 		array.(c).(d) <- deux_ou_quatre;
 		array_prime.(a).(b) <- 1;
-		array_prime.(c).(d) <- 1;
-		moveto 20 20;
-		set_color white;
-		draw_string (string_of_int a);
-		moveto 50 20;
-		set_color white;
-		draw_string (string_of_int b);;
-
-let test = 
-let f = make_matrix 4 4 0 and g = make_matrix 4 4 0 in
-debut f g;
-read_print f g;;
-
-
-(* Structures pour les blocs : sous la forme d'un tableau (listes) *)
-
-
-
+		array_prime.(c).(d) <- 1;;
 (* Fonctions qui modifie le tableau + score *)
 
 (* Fonctions qui lit le tableau et l'affiche + affiche le background *)
 	
+let print_score score = 
+		set_color white;
+		set_text_size 25;
+		moveto 35 200;
+		draw_string (string_of_int score);;
+	
+		(* fonctions qui détermine la couleur de chaque case associée 
+ainsi que le son entendu lors d'une collision *)
+
+let couleur_case nombre = match nombre with
+		| 2 -> rouge
+		| 4 -> vert
+		| 8 -> yellow
+		| 16 -> black
+		| 32 -> bleu
+		| 64 -> bleu_clair
+		| 128 -> rose
+		| 256 -> vert_clair
+		| 512 -> bleu_fonce
+		| 1024 -> violet 
+		| 2048 -> orange
+		| _ -> bleu_ciel;;
+					
+
+		
+let son_collision nombre = match nombre with
+		| 2 -> la
+		| 4 ->
+		| 8 -> 
+		| 16 -> 
+		| 32 -> 
+		| 64 ->
+		| 128 ->
+		| 256 ->
+		| 512 -> 
+		| 1024 ->
+		| 2048 -> 
+		| 4096 -> 
+		;;
+		
+		
+let taille_nombre nb x y = match nb with
+		| n when n < 10 -> moveto (x+60) (y+50)
+		| n when n < 100 -> moveto (x+45) (y+50)
+		| n when n < 1000 -> moveto (x+25) (y+50)
+		| _ -> set_text_size 40; moveto (x+23) (y+55);;
+		
+
 	
 		(* affiche chaque case + nombre *)
 let print_cases x y nombre =
-		moveto x y;
-		set_color bleu;
-		fill_rect x y 140 130;
-		moveto (x+55) (y+45);
+		set_color (couleur_case nombre);
+		fill_rect (x+10) (y+10) 130 130;
 		set_color white;
+		set_text_size 50;
+		taille_nombre nombre x y;
 		draw_string (string_of_int nombre)
 		;;
+				
 
 let read_print array array_prime = 
     (* affiche case et nombre*)
-    set_text_size 50;
     for i = 0 to 3 do 
         for j = 0 to 3 do
             begin 
             if array_prime.(i).(j) = 1
-            then print_cases (150+150*(i mod 4)+20) (450-150*(j mod 4)+10) array.(i).(j)
+            then print_cases (200 + i*150) (450 - j*150) array.(i).(j)
             else ()
             end
         done
     done
     ;;
 
-read_print (make_matrix 4 4 8) (make_matrix 4 4 1);;
+let test = let f = make_matrix 4 4 0 and g = make_matrix 4 4 0 in
+		debut f g;
+		read_print f g;;
+		
+read_print (make_matrix 4 4 2048) (make_matrix 4 4 1);;
 
 (* Fonctions qui choisit un emplacement libre random et choisit 2 ou 4 *)
 		
@@ -128,7 +167,7 @@ let new_coord a b array array_prime =
 		
 let new_tile array array_prime= let t = ref true in 
 		while !t do
-				let a = zero_a_trois and b = zero_a_trois in
+				let a = int 4 and b = int 4 in
 				if array_prime.(a).(b) = 0 then new_coord a b array array_prime else ()
 		done;;
 		
@@ -197,10 +236,10 @@ read_key;;
 type direction = Nord | Sud | Est | Ouest ;;
 
 let trad_direction dir = match dir with
-		| Nord -> 
-		| Sud -> 
-		| Est -> 
-		| Ouest -> 
+		| Nord -> haut
+		| Sud -> bas
+		| Est -> droite
+		| Ouest -> gauche
 ;;	
 
 let read_dir = 
